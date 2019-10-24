79290a003f9738257043d92bf08bb4452f2db412
Krivobarac <gasparovicsasa@yahoo.com>
2019-10-24 15:45:31 +0200

    I have changed to calling Router with Redirect, added more empty pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -11,36 +11,22 @@ import NotFound from '../pages/notFound/NotFound'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import NewCredentials from '../newCredentials/NewCredentials';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: null,
-      password: null,
-      user: null,
-      data: null,
-    }
-  }
-
-  render() {
-    return <div className="App">
-              <Welcome />
-              <Router>
-                <Switch>
-                    <Route exact path='/' component={() => <Login />} />)
-                    <Route path='/newCredentials' component={() => <NewCredentials />} />)
-                    <Route path='/superadmin' component={(props) => <SuperAdmin {...props} />} />
-                    <Route path='/director' component={(props) => <Director {...props} />} />
-                    <Route path='/director' component={(props) => <Teacher {...props} />} />
-                    <Route path='/parrent' component={(props) => <Parrent {...props} />} />
-                    <Route path='/student' component={(props) => <Student {...props} />}  />
-                    <Route component={NotFound} />
-                </Switch>
-              </Router>
-            </div>
-  }
-              
+function App() {
+  return <div className="App">
+            <Welcome />
+            <Router>
+              <Switch>
+                  <Route exact path='/' component={() => <Login />} />)
+                  <Route path='/newCredentials' component={() => <NewCredentials />} />
+                  <Route path='/superadmin' component={(props) => <SuperAdmin {...props} />} />
+                  <Route path='/director' component={(props) => <Director {...props} />} />
+                  <Route path='/director' component={(props) => <Teacher {...props} />} />
+                  <Route path='/parrent' component={(props) => <Parrent {...props} />} />
+                  <Route path='/student' component={(props) => <Student {...props} />}  />
+                  <Route component={NotFound} />
+              </Switch>
+            </Router>
+          </div>
 }
-
 
 export default App;
